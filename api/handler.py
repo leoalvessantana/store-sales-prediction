e9bfdab0d88c3720caac9b49fc710839500952cf
leoalvessantana <leonardoalvs12@gmail.com>
2023-06-29 13:13:25 -0300

    deploy model to prodution

---
+++ api/handler.py
@@ -0,0 +1,48 @@
+import pickle
+
+import pandas as pd
+from flask import Flask, Response, request
+from rossmann.Rossmann import Rossmann
+
+# loading model
+model = pickle.load( open( 'model/model_rossmann.pkl', 'rb') )
+
+# initialize API
+app = Flask( __name__ )
+@app.route( '/rossmann/predict', methods=['POST'] )  # craindo a rota "endpoint" , recebe apenas o metodo POST
+
+def rossmann_predict():
+
+    # pegando o dado
+    test_json = request.get_json()
+
+    if test_json: # caso tenha dado
+        
+        if isinstance( test_json, dict ): # unique example
+            test_raw = pd.DataFrame( test_json, index=[0] )
+        else: # multiple example
+            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
+
+        # Instantiate Rossmann class
+        pipeline = Rossmann()
+
+        # data cleaning
+        df1 = pipeline.data_cleaning( test_raw )
+
+        # feature engineering
+        df2 = pipeline.feature_engineering( df1 )
+
+        # data preparation
+        df3 = pipeline.data_preparation( df2 )
+
+        # prediction
+        df_response = pipeline.get_prediction( model, test_raw, df3 ) 
+
+        return df_response
+
+    else: # caso não tenha dado, retorna vazio
+        return Reponse( '{}', status=200, mimetype='application/json' )
+
+
+if __name__ == '__main__':
+    app.run( '0.0.0.0' )  # roda o Flask localmente
